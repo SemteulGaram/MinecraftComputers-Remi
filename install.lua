f0c35ea3a1a3e5e20114e0a6e5a7eab883676257
SemteulGaram <scgtdy7151@gmail.com>
2020-04-23 00:20:49 +0900

    Fix: install.lua

--- install.lua
+++ install.lua
@@ -1,31 +1,33 @@
-local Comp = require("component")
-local Inet = nil;
+local URLROOT='https://raw.githubusercontent.com/SemteulGaram/OpenComputers-Remi/master/'
+
+local comp = require 'component'
+local inet = nil;
 
 function setup()
-  if not Inet.isAvailable() then
-    print("Internet component not available. Please insert internet card.")
+  if not comp.isAvailable('internet') then
+    print('Internet component not available. Please insert internet card.')
     return 1
   end
-  Inet = Comp.internet
-  if not Inet.isHttpEnabled() {
-    print("This server didn't allow http access. Please download script manually(via pastebin or wget).")
+  inet = comp.getPrimary('internet')
+  if not inet.isHttpEnabled() then
+    print('This server didn\'t allow http access. Please download script manually(via pastebin or wget).')
     return 2
-  }
+  end
   return 0
 end
 
 function install()
-  local req = Inet.request("https://raw.githubusercontent.com/SemteulGaram/OpenComputers-Remi/master/src/main/autorun.lua")
-  local file = io.open("autorun.lua", "w")
-  local data = req.read()
+  local req=inet.request(URLROOT..'dirlist.json')
+  local file=io.open("autorun.lua", "w")
+  local data=req.read()
   while data do
     file:write(data)
-    data = req.read()
+    data=req.read()
   end
   file:close()
   return true
 end
 
-if setup()>0 then
+if setup()==0 then
   install()
 end
