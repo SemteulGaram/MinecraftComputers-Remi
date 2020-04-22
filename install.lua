3eceed0d54f5f9edeb2a3f5c46f8f3bf92c65a5c
SemteulGaram <scgtdy7151@gmail.com>
2020-04-23 01:06:30 +0900

    Add: gitTrans install

--- install.lua
+++ install.lua
@@ -1,14 +1,16 @@
-local URLROOT='https://raw.githubusercontent.com/SemteulGaram/OpenComputers-Remi/master/'
+--Very First install only script
+--It will install "gitTrans" which download/upload manager from github code
+local GITTRANSURL='https://raw.githubusercontent.com/SemteulGaram/OpenComputers-Remi/master/src/gitTrans.lua'
 
 local comp = require 'component'
-local inet = nil;
+local inet = nil
 
-function setup()
+local function check()
   if not comp.isAvailable('internet') then
     print('Internet component not available. Please insert internet card.')
     return 1
   end
-  inet = comp.getPrimary('internet')
+  inet=comp.getPrimary('internet')
   if not inet.isHttpEnabled() then
     print('This server didn\'t allow http access. Please download script manually(via pastebin or wget).')
     return 2
@@ -16,9 +18,12 @@ function setup()
   return 0
 end
 
-function install()
-  local req=inet.request(URLROOT..'dirlist.json')
-  local file=io.open("autorun.lua", "w")
+local function install()
+	--Ensure Dir
+  xpcall(function()os.execute('mkdir src')end, function(err)end)
+
+  local req=inet.request(GITTRANSURL)
+  local file=io.open("src/gitTrans.lua", "w")
   local data=req.read()
   while data do
     file:write(data)
@@ -28,6 +33,23 @@ function install()
   return true
 end
 
-if setup()==0 then
-  install()
+print('Remi check...')
+local code = check()
+if code~=0 then
+  print('setup process terminated with error code: '..code)
+  return
+end
+
+print('Remi install...')
+code=install()
+if code~=0 then
+  print('install process terminated with error code: '..code)
+  return
+end
+
+print('gitTrans force pulling...')
+local gitTrans=require './src/gitTrans.lua'
+code=gitTrans.forcePull()
+if code~=0 then
+  print('gitTrans.forcePull() process terminated with error code: '..code)
 end
